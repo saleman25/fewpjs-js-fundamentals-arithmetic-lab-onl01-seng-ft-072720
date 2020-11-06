24d544a1126d8b01fd766471bcb83b351e223f79
Samantha Aleman <samanthaaleman@samanthas-air.home>
2020-11-05 20:05:12 -0800

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 
 // Write your code here
+
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2 ;
+const random = Math.random();
+const mod = 20 % 8;
+const max = Math.max(5, 10, 15, 20);
